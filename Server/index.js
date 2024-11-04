68497ccb0483f324ef91ca2e684341abd91b565d
ChandaniSahu <rrrsahu2005@gmail.com>
2024-11-04 15:20:02 +0530

    cors syntax error is resolved

--- Server/index.js
+++ Server/index.js
@@ -4,11 +4,14 @@ const router = require('./Router/router.js')
 const connectMongo = require('./db.js')
 const cors = require('cors')
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://ebook-chandani.netlify.app'],
+    credentials: true
+}))
 app.use(express.json())
-app.use('/api',router)
+app.use('/api', router)
 connectMongo()
-const port=3000
-app.listen(port,()=>{
- console.log('server is listening on port : ',port)
+const port = 3000
+app.listen(port, () => {
+    console.log('server is listening on port : ', port)
 })
